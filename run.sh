0dba56c4baa5a835cd72858f54aa012feaaa8aab
sebashack <sebastian0092@gmail.com>
2021-02-21 16:40:17 -0500

    Remove specific user

--- run.sh
+++ run.sh
@@ -5,10 +5,8 @@ set -xeuf -o pipefail
 ROOT="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )" )"
 DOCKER_FILE="${ROOT}/docker-compose.yaml"
 DATA_DIR="${ROOT}/data"
-USER_ID="$( id -u $USER )"
 
 cp -f  docker-compose-template.yaml docker-compose.yaml
 sed -i "s|<path-to-data-dir>|${DATA_DIR}|g" docker-compose.yaml
-sed -i "s|<db-user>|${USER_ID}|g" docker-compose.yaml
 
 docker-compose --file "$DOCKER_FILE" up --detach --remove-orphans
