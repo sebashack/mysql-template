98e2a52039d2366784c82d2f267d27e2477e2437
sebashack <sebastian0092@gmail.com>
2021-04-09 23:17:09 -0500

    More debugging

--- scripts/test-2/queries/query1.sql
+++ scripts/test-2/queries/query1.sql
@@ -1,10 +1,9 @@
 USE soundspot_db;
 
--- Query all second season's participants by 'Mariana Shostakovich'
+-- Query all second season's participants of show 'Marriage Life'
 
-SELECT DISTINCT c.name AS host, p.name AS participant FROM participant p, podcast_episode pe, `show` s, content_creator c
-WHERE c.name = 'Mariana Shostakovich'
+SELECT DISTINCT p.name AS participant FROM participant p, podcast_episode pe, `show` s
+WHERE s.name = 'Marriage Life'
   AND pe.season_number = 2
   AND p.podcast_episode_id = pe.id
-  AND s.content_creator_id = c.id
   AND s.id = pe.show_id;
